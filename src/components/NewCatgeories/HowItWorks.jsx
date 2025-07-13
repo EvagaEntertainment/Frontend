e456edeb2fd000586af482f6d88fd1e7f7994538
arman8808 <armanal3066@gmail.com>
2025-07-13 16:15:57 +0530

    chnage the styling

--- src/components/NewCatgeories/HowItWorks.jsx
+++ src/components/NewCatgeories/HowItWorks.jsx
@@ -137,7 +137,7 @@ function NewCatgeories() {
       </motion.div>
 
       <motion.div
-        className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 w-full px-4 sm:px-6 lg:px-8"
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full px-4 sm:px-6 lg:px-8"
         variants={containerVariants}
       >
         {data.map((item, index) => (
