2bd018387910ef5e95d359dba7cc099d8debb25e
arman8808 <armanal3066@gmail.com>
2026-04-02 21:27:59 +0530

    feat: add new pages, sitemap, and robots configuration to improve site structure and SEO

--- src/pages/HomepageNew.jsx
+++ src/pages/HomepageNew.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async'
 import SliderNew from '../components/Slider/SliderNew'
 import NewCatgeories from '../components/NewCatgeories/HowItWorks'
 import HowItWorks from '../components/HowItWorks/HowItWorks'
@@ -10,7 +11,27 @@ import BookingSection from '../components/BookingSection/BookingSection'
 
 function HomepageNew() {
   return (
-    <div className='flex flex-col '>
+    <>
+      <Helmet>
+        <title>Eevagga | India's Premium Birthday Celebration Platform</title>
+        <meta
+          name="description"
+          content="Discover India's first premium birthday celebration platform. We offer curated birthday experiences, products, and services to make your birthday special."
+        />
+        <meta
+          name="keywords"
+          content="birthday party planning, birthday decorators, birthday gifts, premium birthday experiences, Eevagga, birthday packages"
+        />
+        <link rel="canonical" href={window.location.origin + window.location.pathname} />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Eevagga | India's Premium Birthday Celebration Platform" />
+        <meta
+          property="og:description"
+          content="Discover India's first premium birthday celebration platform. We offer curated birthday experiences, products, and services to make your birthday special."
+        />
+        <meta property="og:type" content="website" />
+      </Helmet>
+      <div className="flex flex-col ">
         <SliderNew/>
         <NewCatgeories/>
         <WhyPeopleTrustEvaga/>
@@ -22,6 +43,7 @@ function HomepageNew() {
         {/* <OurWorkSection/> */}
         <FAQSection/>
     </div>
+    </>
   )
 }
 
